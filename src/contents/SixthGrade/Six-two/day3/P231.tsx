750628792f86dc10466f8c66e8d0e8453882d7d2
eunbenn <ds5bean23@gmail.com>
2024-01-31 19:05:55 +0900

    fix: 주석해제

--- src/contents/SixthGrade/Six-two/day3/P231.tsx
+++ src/contents/SixthGrade/Six-two/day3/P231.tsx
@@ -29,8 +29,8 @@ export default function P231() {
 
   const checkAnswer = async () => {
     const currentScore = calculateTruePercentage(passArray);
-    // if (!isSolved) await sendScoreUtil(currentScore);
-    // await postKeyValue({ key: 'quiz231.answer', value: allAnswer });
+    if (!isSolved) await sendScoreUtil(currentScore);
+    await postKeyValue({ key: 'quiz231.answer', value: allAnswer });
     setIsSolved(prev => !prev);
   };
 
